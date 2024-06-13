22b1f250f1f3ba3ef0b8729e868bf66c71ff0afd
RomanBuzz <buninrom@gmail.com>
2024-06-13 23:56:17 +0300

    intervalId name correction
    intervalId name correction

--- E3_task4.js
+++ E3_task4.js
@@ -1,10 +1,10 @@
 function everySecondOutput(num1, num2) {
     let counter = num1;
-    let timerId = setInterval(() => {
+    let intervalId = setInterval(() => {
         if (counter <= num2) {
             console.log(counter);
             counter++;
-        } else clearInterval(timerId);
+        } else clearInterval(intervalId);
     }, 1000);
 }
 
